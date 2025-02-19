ff53cc28a6b6bce7d171a9903eb68c06a93d8efc
edeldev <edeldev25@gmail.com>
2025-02-18 18:14:48 -0600

    chore: add new link

--- src/utils/Enlaces.js
+++ src/utils/Enlaces.js
@@ -1,9 +1,14 @@
-import { IconBrandYoutube } from "@tabler/icons-react";
+import { IconBrandYoutube, IconAppWindow } from "@tabler/icons-react";
 
 export const MEDIA_ENLACES = [
   {
     url: "https://youtube.com/shorts/ol1_jOD3SHI?si=CYmpzglsiCvabGmB",
     icon: IconBrandYoutube,
     texto: "Último video de YouTube",
+  },
+  {
+    url: "https://wa.me/528123697420?text=Hola,%20quiero%20una%20página%20web",
+    icon: IconAppWindow,
+    texto: "Quiero una página web",
   },
 ];
